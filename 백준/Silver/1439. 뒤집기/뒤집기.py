931e8770976a6802035480959f3ffe480ab28c99
cmgjn1881 <127597947+cmgjn1881@users.noreply.github.com>
2024-08-04 13:33:49 +0900

    [Silver V] Title: 뒤집기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -0,0 +1,15 @@
+import sys
+
+S = sys.stdin.readline().rstrip()
+cnt = 0
+
+for i in range(1, len(S)):
+    if S[i] != S[i - 1]:
+        cnt += 1
+
+if cnt == 1:
+    print(1)
+elif cnt % 2 == 0:
+    print(cnt // 2)
+else:
+    print((cnt + 1) // 2)
