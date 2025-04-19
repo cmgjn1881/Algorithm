96b19c87c0482e0c92611486f6245fac149f6325
cmgjn1881 <127597947+cmgjn1881@users.noreply.github.com>
2025-04-19 20:53:11 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 484 ms, Memory: 294204 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,47 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int count = 0;
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        StringBuilder result = new StringBuilder("<");
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= N; i++) {
+            deque.add(i);
+        }
+
+        while (!deque.isEmpty()) {
+            count++;
+
+            //조건문 방법
+//            int x = deque.removeFirst();
+//            if (count % K == 0) {
+//                result.append(x).append(", ");
+//            } else {
+//                deque.add(x);
+//            }
+
+            //반복문 방법
+            for (int i = 0; i < K - 1; i++) {
+                int x = deque.removeFirst();
+                deque.add(x);
+            }
+            result.append(deque.removeFirst()).append(", ");
+
+        }
+        result.delete(result.length() - 2, result.length());
+        result.append(">");
+        System.out.println(result);
+    }
+}
