dd24b9bf95cb81a1d6f84262d827f7beade60d74
cmgjn1881 <127597947+cmgjn1881@users.noreply.github.com>
2025-04-29 13:42:03 +0900

    [Silver III] Title: N번째 큰 수, Time: 744 ms, Memory: 273712 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -6,23 +6,30 @@ import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/2075
 // 2075번 - N번째 큰 수
-public class Main { // 제출시 Main
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // N을 입력 받음
         int N = Integer.parseInt(br.readLine());
+        // 최소 힙을 우선순위큐로 구현
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         
         for (int i = 0; i < N; i++) {
+		        // StringTokenizer를 사용하여 한 줄을 입력 받음
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
+		            // N번째로 큰 수를 빼기 위해 숫자 앞에 '-'를 추가 후 힙에 저장
                 minHeap.add(-Integer.parseInt(st.nextToken()));
             }
         }
 
+				// N - 1번째 큰 수까지 제거
         for (int i = 0; i < N - 1; i++) {
             minHeap.poll();
         }
 
+				// N번째로 큰 수 출력
         System.out.println(-minHeap.poll());
     }
 }
