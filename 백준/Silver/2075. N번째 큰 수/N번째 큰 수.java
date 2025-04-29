db2ed45d14e98734604045d370f369c7f237076c
cmgjn1881 <127597947+cmgjn1881@users.noreply.github.com>
2025-04-29 10:06:21 +0900

    [Silver III] Title: N번째 큰 수, Time: 756 ms, Memory: 274040 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,32 +1,29 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/2075
 // 2075번 - N번째 큰 수
-public class Main {
+public class Main { // 제출시 Main
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N * N];
-        int idx = 0;
-
-        // List에 입력값 추가하기
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            while (st.hasMoreTokens()) {
-                arr[idx++] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                minHeap.add(-Integer.parseInt(st.nextToken()));
             }
         }
-        // List 정렬
-        Arrays.sort(arr);
-        // N 번째로 큰 수의 인덱스를 이용해 출력
-        System.out.println(arr[arr.length - N]);
+
+        for (int i = 0; i < N - 1; i++) {
+            minHeap.poll();
+        }
+
+        System.out.println(-minHeap.poll());
     }
 }
 
