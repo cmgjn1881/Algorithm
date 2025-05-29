6016e41be4f20adb3ccef9fac5a71ff881b10a76
cmgjn1881 <127597947+cmgjn1881@users.noreply.github.com>
2025-05-29 22:19:02 +0900

    [level 3] Title: 단어 변환, Time: 1.09 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -11,28 +11,28 @@ class Solution {
     }
     
     int diffCount(String word1, String word2) {
-        int cnt = 0;
+        int count = 0;
         for (int i = 0; i < word1.length(); i++) {
             if (word1.charAt(i) != word2.charAt(i)) {
-                cnt++;
+                count++;
             }
         }
-        return cnt;
+        return count;
     }
     
     public int solution(String begin, String target, String[] words) {
         boolean[] visited = new boolean[words.length];
-        
         Queue<Word> q = new ArrayDeque<>();
         q.offer(new Word(begin, 0));
         
         while(!q.isEmpty()) {
             Word cur = q.poll();
             
-            if (cur.word.equals(target)) return cur.count;
+            if (cur.word.equals(target)) {
+                return cur.count;
+            }
             
             for (int i = 0; i < words.length; i++) {
-                
                 if (diffCount(cur.word, words[i]) == 1 && !visited[i]) {
                     q.offer(new Word(words[i], cur.count + 1));
                     visited[i] = true;
