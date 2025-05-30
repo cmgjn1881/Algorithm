fbe0342fd37d9c6491a81179413e20ad044d7e6a
cmgjn1881 <127597947+cmgjn1881@users.noreply.github.com>
2025-05-30 09:20:47 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.99 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -5,10 +5,10 @@ class Solution {
         int n = maps.length;
         int m = maps[0].length;
         
+        boolean[][] visited = new boolean[n][m];
+        
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
-        
-        boolean[][] visited = new boolean[n][m];
         
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{0, 0, 1});
@@ -22,10 +22,9 @@ class Solution {
             for (int i = 0; i < 4; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
-                
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m &&
-                   maps[nx][ny] == 1 && !visited[nx][ny]) {
-                    q.offer(new int[] {nx, ny, cur[2] + 1});
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && 
+                   !visited[nx][ny] && maps[nx][ny] == 1) {
+                    q.offer(new int[]{nx, ny, cur[2] + 1});
                     visited[nx][ny] = true;
                 }
             }
